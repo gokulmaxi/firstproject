f9f19217095331857ddacf5872c3443fce6fe5d8
gokulmaxi <gokulakams@gmail.com>
2023-03-21 11:39:52 +0530

    Add project files.

---
+++ firstproject/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace firstproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            do
+            {
+                Console.WriteLine(a);
+                a--;
+            } while (a > 5);
+        }
+    }
+}
