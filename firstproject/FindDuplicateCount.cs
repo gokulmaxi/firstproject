92ae1fc28dae12df2922265743d1806502333f6d
gokulmaxi <gokulakams@gmail.com>
2023-03-23 17:02:00 +0530

    asses: FInd duplicate count in array

---
+++ firstproject/FindDuplicateCount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    internal class FindDuplicateCount<T>
+    {
+        T[] arr;
+        public FindDuplicateCount(T[] arr)
+        {
+            this.arr = arr;
+        }
+        public int GetCount()
+        {
+            int dupCount = 0;
+            Hashtable hashtable = new Hashtable();
+            foreach (var item in arr)
+            {
+                if (hashtable.ContainsKey(item))
+                {
+                    hashtable[item] = (int)hashtable[item]+1;
+                }
+                else
+                {
+                    hashtable.Add(item, 0);
+                }
+            }
+            foreach (var item in hashtable.Keys)
+            {
+                dupCount = (int)hashtable[item] + dupCount;
+            }
+            return dupCount;
+        }
+    }
+}
--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -4,20 +4,12 @@
     {
         static void Main(string[] args)
         {
-            AssesHero hero = new AssesHero("hero",50,1);
-            AssesHero villian = new AssesHero("villian",50,3);
-            Random rn = new Random();
-            while (true)
-            {
-                if (rn.Next(2) == 1)
-                {
-                    if(hero.shoot())break;
-                }
-                else
-                {
-                    if(villian.shoot())break;
-                }
-            }
+            int[] arr = { 1, 2, 3, 4,1,2};
+            char[] charArr = { 'a', 'b','b'};
+            FindDuplicateCount<int> findDupCount = new FindDuplicateCount<int>(arr);
+            Console.WriteLine( findDupCount.GetCount());
+            FindDuplicateCount<char> findDupCountChar = new FindDuplicateCount<char>(charArr);
+            Console.WriteLine( findDupCountChar.GetCount());
         }
     }
 }
