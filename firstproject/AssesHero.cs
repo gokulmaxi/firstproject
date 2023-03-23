929fd737fbc66ac4a8edf77032a632bc6ec766df
gokulmaxi <gokulakams@gmail.com>
2023-03-23 16:28:16 +0530

    Completed assesment of hero and villian shooting

---
+++ firstproject/AssesHero.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    internal class AssesHero
+    {
+        string name;
+        int bulletCount;
+        int bulletDecrement;
+        public AssesHero(string name,int bulletCount,int bulletDecrement) {
+            this.name = name;
+            this.bulletCount = bulletCount;
+            this.bulletDecrement = bulletDecrement;
+        }
+        public bool shoot()
+        {
+            if (this.bulletCount > this.bulletDecrement)
+            {
+                this.bulletCount = this.bulletCount - this.bulletDecrement;
+                Console.WriteLine(name + " has shot with " + bulletDecrement + " bullets");
+                Console.WriteLine("Now " + name + " has " + bulletCount + " left");
+                Console.WriteLine("=====>");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine(name+" has no more bullets to shoot");
+                return true;
+            }
+        }
+    }
+}
--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            QueueEx queueEx = new QueueEx();
+            AssesHero hero = new AssesHero("hero",50,1);
+            AssesHero villian = new AssesHero("villian",50,3);
+            Random rn = new Random();
+            while (true)
+            {
+                if (rn.Next(2) == 1)
+                {
+                    if(hero.shoot())break;
+                }
+                else
+                {
+                    if(villian.shoot())break;
+                }
+            }
         }
     }
 }
